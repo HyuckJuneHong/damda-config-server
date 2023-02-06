c34dfea9c1b96c4233cd8ad0a628a36a137b90a0
홍혁준 <jjj8337721@naver.com>
2023-02-06 14:47:00 +0900

    first config server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # damda-config-server
+> MSA 기반의 사이드 프로젝트입니다.
+> Config Server 입니다.
---
+++ src/main/java/kr/co/damdaconfig/DamdaConfigApplication.java
@@ -0,0 +1,13 @@
+package kr.co.damdaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamdaConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamdaConfigApplication.class, args);
+    }
+
+}
